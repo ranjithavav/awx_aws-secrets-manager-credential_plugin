0ce7d8b24d472e2bae13099ec1129880ad014da4
RANJITH A V <33387735+ranjithavav@users.noreply.github.com>
2024-02-16 18:03:23 +0530

    modified name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 requirements = ["boto3", "botocore" ] 
 setup(
-    name='awx_aws-secrets-manager-credential_plugin',
+    name='awx_aws_secrets_manager_credential_plugin',
     version='0.1',
     author='Ansible, Inc.',
     author_email='info@ansible.com',
@@ -11,14 +11,14 @@ setup(
     license='Apache License 2.0',
     keywords='ansible',
     url='https://github.com/ranjithavav/awx_aws-secrets-manager-credential_plugin',
-    packages=['awx_aws-secrets-manager-credential_plugin'],
+    packages=['awx_aws_secrets_manager_credential_plugin'],
     include_package_data=True,
     zip_safe=False,
     setup_requires=[],
     install_requires=requirements,
     entry_points = {
         'awx.credential_plugins': [
-            'aws_cred_role_plugin = awx_aws-secrets-manager-credential_plugin:aws_cred_role_plugin',
+            'aws_cred_role_plugin = awx_aws_secrets_manager_credential_plugin:aws_cred_role_plugin',
         ]
     }
 )
